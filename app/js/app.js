88a3939355c81a59bb1f5b222c664e922d3a5715
evgenij <mwbox89@gmail.com>
2021-03-18 21:55:00 +0200

    begin mob of main section

--- app/js/app.js
+++ app/js/app.js
@@ -10,4 +10,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
 
+	function switchLang () {
+		let selectedLang = document.querySelector('.switch-lang__selected')
+		let selectedLangInner = document.querySelector('.switch-lang__selected--inner')
+		let hiddenList = document.querySelector('.switch-lang__list')
+		let parentSwith = document.querySelector('.switch-lang')
+		parentSwith.addEventListener('click', function(e) {
+
+			if (e.target === selectedLang || e.target === selectedLangInner) {
+				e.currentTarget.classList.toggle('open')
+			} else if (e.target.classList.contains('switch-lang__btn')) {
+				selectedLangInner.innerHTML = e.target.innerText
+				e.currentTarget.classList.remove('open')
+			}
+			
+		})
+	}
+	switchLang ()
 })
